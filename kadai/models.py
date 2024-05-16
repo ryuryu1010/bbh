0c2e84515f3e3e80a7b3b28862ec51c3d4f57efd
ryuryu1010 <211015@std.hi-joho.ac.jp>
2024-05-16 10:42:14 +0900

    OK

--- kadai/models.py
+++ kadai/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 
+#他病院表
+
 class Tabyouin(models.Model):
     tabyouinid = models.CharField(max_length=8, primary_key=True)
     tabyouinmei = models.CharField(max_length=64)
@@ -10,7 +12,7 @@ class Tabyouin(models.Model):
     byouinshihonk = models.IntegerField()
     kyukyu = models.IntegerField()
 
-
+#仕入れ先表
 
 class Shiiregyosha(models.Model):
     shiireid = models.CharField(max_length=8, primary_key=True)
@@ -20,6 +22,7 @@ class Shiiregyosha(models.Model):
     shihonkin = models.IntegerField()
     nouki = models.IntegerField()
 
+#従業員表
 
 class Employee(models.Model):
     empid = models.CharField(max_length=8, primary_key=True)
@@ -28,7 +31,7 @@ class Employee(models.Model):
     emppasswd = models.CharField(max_length=64)
     emprole = models.IntegerField()
 
-
+#患者表
 
 class Patient(models.Model):
     patid = models.CharField(max_length=8, primary_key=True)
@@ -37,7 +40,7 @@ class Patient(models.Model):
     hokenmei = models.CharField(max_length=64)
     hokenexp = models.DateField()
 
-
+#薬剤表
 
 class Medicine(models.Model):
     medicineid = models.CharField(max_length=8, primary_key=True)
